89b2e77a86b047aa85772d74b5aee2b6114b9948
zemariolacerda <zemariolacerda@gmail.com>
2020-06-04 17:21:07 -0300

    Classe Usuario

---
+++ app/src/main/java/com/plagesribeiro/pucantina/Usuario.java
@@ -0,0 +1,48 @@
+package com.plagesribeiro.pucantina;
+
+public class Usuario {
+    private String nome, email, telefone, curso, id;
+
+
+    public Usuario () {}
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getCurso() {
+        return curso;
+    }
+
+    public void setCurso(String curso) {
+        this.curso = curso;
+    }
+}
