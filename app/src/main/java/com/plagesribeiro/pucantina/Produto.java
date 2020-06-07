ed6a77d70e90e81d89af4b2f4064722aac192e55
zemariolacerda <zemariolacerda@gmail.com>
2020-06-07 00:40:32 -0300

    Criação da Tela do usuário

---
+++ app/src/main/java/com/plagesribeiro/pucantina/Produto.java
@@ -0,0 +1,40 @@
+package com.plagesribeiro.pucantina;
+
+
+public class Produto {
+    private String valor, nome, descricao, imagem;
+
+    public void Produto() {}
+
+    public String getValor() {
+        return valor;
+    }
+
+    public void setValor(String valor) {
+        this.valor = valor;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getImagem() {
+        return imagem;
+    }
+
+    public void setImagem(String imagem) {
+        this.imagem = imagem;
+    }
+}
